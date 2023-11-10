079141b649543a5d2bea77d7e77a8f93ee171827
crisspreceadance <150423499+crisspreceadance@users.noreply.github.com>
2023-11-10 03:28:22 -0400

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,26 @@
+
+var numero2 = 100
+var cadena = "Hola,mundo soy Criss, de Diseño Grafico,y no soy Carlos"
+
+
+
+
+
+console.log(numero2);
+console.log(cadena.length);
+console.log(cadena.charAt(cadena.length-1));
+console.log(cadena.indexOf("s"));
+console.log(cadena.lastIndexOf("s"));
+console.log(cadena.replace("criss" , "carlos"));
+console.log(cadena.search("de"));
+console.log(cadena.split(","));
+
+console.log(cadena.toLowerCase());
+console.log(cadena.toUpperCase());
+console.log(cadena.trim());
+console.log(cadena.concat(" ","del"," ", "CEA"," ", "DON BOSCO"));
+cadena = cadena +" "+"del"+" "+"turno"+" "+"tarde"
+console.log(cadena);
+
+console.log(numero2.toString());
+console.log(numero2+" Criss");
